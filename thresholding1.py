d0878b5617f4ec30e330cc95fd9b35ce5a235f31
Mohsin Riaz <wilddogers@gmail.com>
2020-06-28 01:41:11 +0200

    Thresholding

---
+++ thresholding1.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('gradient.png', 0)
+
+# binary threshold -> either 0 if blow min value and max value if above min
+_, th_binary = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+
+# inverse binary threshold
+_, th_binary_inv = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
+
+
+
+# trunc threshold -> apply min value after the min value in image
+_, th_trunc = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
+
+
+# To Zero threshold -> if pixel val < min then img val=0
+_, th_to_zero = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
+
+# To Zero inverse threshold
+_, th_to_zero_inv = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
+
+
+cv.imshow('image', img)
+cv.imshow('Threshold', th_to_zero_inv)
+
+cv.waitKey(0) 
+cv.destroyAllWindows()
+    
---
+++ thresholding2.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import numpy as np
+
+def tb_cb(x):
+    pass
+
+img = cv.imread('sudoku.png', 0)
+
+cv.namedWindow('params')
+cv.createTrackbar('maxValue', 'params', 0, 255, tb_cb)
+cv.createTrackbar('blockSize', 'params', 11, 256, tb_cb)
+cv.createTrackbar('constant', 'params', 0, 512, tb_cb)
+
+
+while True:
+
+    _mv = cv.getTrackbarPos('maxValue', 'params')
+    _bs = cv.getTrackbarPos('blockSize', 'params')
+    _c = cv.getTrackbarPos('constant', 'params')
+
+    if _bs % 2 == 0:
+        _bs = _bs + 1
+
+    adoptive_thresh_mean = cv.adaptiveThreshold(img, _mv, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, _bs, _c)
+    adoptive_thresh_gaussian = cv.adaptiveThreshold(img, _mv, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, _bs, _c)
+
+    #cv.imshow('image', img)
+    cv.imshow('Threshold Mean', adoptive_thresh_mean)
+    cv.imshow('Threshold Gausian', adoptive_thresh_gaussian)
+
+    k = cv.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+cv.destroyAllWindows()
+    
