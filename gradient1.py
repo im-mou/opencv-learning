333affbc1455e5c1f1bab5c2a7d2ab3a9cbd5bd7
Mohsin Riaz <wilddogers@gmail.com>
2020-06-28 23:09:06 +0200

    blur + gradients

---
+++ gradient1.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('sudoku.png', 0)
+
+lap = cv.Laplacian(img, cv.CV_64F, ksize=3)
+# cv.CV_64F datatype -> supports the negative slope value
+lap = np.uint8(np.absolute(lap))
+
+sobelX = cv.Sobel(img, cv.CV_64F, 1, 0)
+sobelY = cv.Sobel(img, cv.CV_64F, 0, 1)
+
+sobelX = np.uint8(np.absolute(sobelX))
+sobelY = np.uint8(np.absolute(sobelY))
+
+sobel_or = cv.bitwise_or(sobelX, sobelY)
+
+#matlibplot windows titles
+titles = ["Original", "Laplacian", "SobelX", "SobelY", "Sobel combined"]
+images = [img, lap, sobelX, sobelY, sobel_or]
+
+for i in range(len(titles)):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
+ 
---
+++ smooth-blur1.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('lena.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+# create kernel => k = kMatrix / (k_h * k_w)
+kernel = np.ones((5, 5), np.float32)/25
+
+# filter "D -> homogeneous"
+dst = cv.filter2D(img, -1, kernel)
+
+# low pass -> removes noise and blurs the image
+# high pass -> helps find images in the image
+
+# blur = 2dFilter = averaging
+blur = cv.blur(img, (5, 5))
+
+# gaussion filter -> weights in the center of the kernal are higher than 
+# the ones away from the kernel
+gblur = cv.GaussianBlur(img, (5, 5), 0)
+
+# median blur -> get the median of the neighbours pixels values
+mblur = cv.medianBlur(img, 5)
+
+# bilateral filter -> edges are preserved better
+bilat = cv.bilateralFilter(img, 9, 75, 75)
+
+# matlibplot windows titles
+titles = ["Original", "2D Convolution", "Blur", "Gaussian", "Median", "BiLat"]
+images = [img, dst, blur, gblur, mblur, bilat]
+
+for i in range(len(titles)):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i]) 
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
